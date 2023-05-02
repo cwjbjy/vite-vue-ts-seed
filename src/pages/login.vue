1b2e95d7ec94f083c0013d8b9a51d888179904ce
cwj <595842570@qq.com>
2023-05-02 15:22:45 +0800

    feat: sass

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,3 +1,9 @@
 <template>
   <div>登录页面</div>
 </template>
+
+<style lang="scss">
+body {
+  color: $test-color;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path"; //这个path用到了上面安装的@types/node
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path'; //这个path用到了上面安装的@types/node
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +8,15 @@ export default defineConfig({
   //这里进行配置别名
   resolve: {
     alias: {
-      "@": path.resolve("./src"), // @代替src
-      "#": path.resolve("./types"), // #代替types
+      '@': path.resolve('./src'), // @代替src
+      '#': path.resolve('./types'), // #代替types
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/styles/index.scss";',
+      },
     },
   },
 });

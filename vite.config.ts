73cd2be58f152a6bd99ac3212d780e48066946e7
cwj <595842570@qq.com>
2023-05-13 19:27:35 +0800

    feat: 在vite.config.ts中读取环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path'; //这个path用到了上面安装的@types/node
 import AutoImport from 'unplugin-auto-import/vite';
@@ -7,59 +7,63 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    {
-      ...viteCompression(),
-      apply: 'build',
-    },
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 8080,
-    open: true,
-    https: false,
-    proxy: {
-      '/api': {
-        target: '要代理的地址',
-        changeOrigin: true,
-        ws: true,
-        rewrite: (path: string) => path.replace(/^\/api/, ''),
+export default ({ mode }) => {
+  // console.log('mode', loadEnv(mode, process.cwd()).VITE_BASE_URL);//使用loadEnv在vite.config.ts中读取环境变量
+  const baseUrl = loadEnv(mode, process.cwd()).VITE_BASE_URL;
+  return defineConfig({
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      {
+        ...viteCompression(),
+        apply: 'build',
       },
-    },
-  },
-  //这里进行配置别名
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'), // @代替src
-      '#': path.resolve('./types'), // #代替types
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/assets/styles/index.scss";',
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'static/js/[name]-[hash].js',
-        entryFileNames: 'static/js/[name]-[hash].js',
-        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
-        manualChunks: {
-          vue: ['vue', 'pinia', 'vue-router'],
-          elementIcons: ['@element-plus/icons-vue'],
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+      open: true,
+      https: false,
+      proxy: {
+        '/api': {
+          target: baseUrl,
+          changeOrigin: true,
+          ws: true,
+          rewrite: (path: string) => path.replace(/^\/api/, ''),
         },
       },
     },
-  },
-});
+    //这里进行配置别名
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'), // @代替src
+        '#': path.resolve('./types'), // #代替types
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/index.scss";',
+        },
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+          manualChunks: {
+            vue: ['vue', 'pinia', 'vue-router'],
+            elementIcons: ['@element-plus/icons-vue'],
+          },
+        },
+      },
+    },
+  });
+};
